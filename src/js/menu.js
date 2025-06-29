badf15690f2296173691250b4f023789d456291b
garkusha <gaa197848@gmail.com>
2025-06-29 20:30:02 +0300

    fixed menu toggle

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,13 +3,15 @@
     // Додати атрибут data-modal-open на кнопку відкриття
     openMenuBtn: document.querySelector("[data-menu-open]"),
     // Додати атрибут data-modal-close на кнопку закриття
-    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    closeMenuBtns: document.querySelectorAll("[data-menu-close]"),
     // Додати атрибут data-modal на бекдроп модалки
     modal: document.querySelector("[data-menu]"),
   };
 
   refsq.openMenuBtn.addEventListener("click", toggleModal);
-  refsq.closeMenuBtn.addEventListener("click", toggleModal);
+
+  for (const btn of refsq.closeMenuBtns)
+    btn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
